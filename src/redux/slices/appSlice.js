4cf69a3e1319cf7afc514e96f70eb7420fd24657
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-09-06 14:40:15 +0300

    Added store.js and appSlice.js

---
+++ src/redux/slices/appSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: {
+        name: "Oleksandr Sidelnikov",
+        email: "alex@example.com",
+        role: "Frontend Developer",
+    },
+    theme: "light",
+};
+
+const appSlice = createSlice({
+    name: "app",
+    initialState,
+    reducers: {
+        toggleTheme: (state) => {
+            state.theme = state.theme === "light" ? "dark" : "light";
+        },
+    },
+});
+
+export const { toggleTheme } = appSlice.actions;
+
+export const selectUser = (state) => state.app.user;
+export const selectTheme = (state) => state.app.theme;
+
+export default appSlice.reducer;
